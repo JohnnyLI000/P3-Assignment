e9642078db21529c8cfbe0dfa82aa17c938bd44a
Johnny Li <30562404+JohnnyLI000@users.noreply.github.com>
2019-05-18 10:56:40 +1200

    Initial commit...

---
+++ src/p3/assignment1/Help.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p3.assignment1;
+
+/**
+ *
+ * @author Yuan Hao Li
+ */
+public class Help {
+    private Player player;
+    public  Help(Player player)
+    {
+        this.player = player;
+    }
+    
+    public void phoneCall()
+    {
+        System.out.println("You can have a phone call with a person to discuss this question");
+    }
+    
+    
+    public void half(String a, String b, String c, String d , String answer)
+    {
+        
+    }
+    
+    public void audience()
+    {
+        System.out.println("You can ask someone about this question");
+    }
+    
+    
+}
