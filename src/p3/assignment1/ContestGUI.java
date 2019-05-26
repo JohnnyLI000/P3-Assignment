59f2b79b20ab61dba4b56dd2950ef0262ca52c11
Johnny Li <dqt4431@autuni.ac.nz>
2019-05-26 22:29:27 +1200

    made the score panel work , need to fix the bug

--- src/p3/assignment1/ContestGUI.java
+++ src/p3/assignment1/ContestGUI.java
@@ -18,14 +18,20 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.GroupLayout;
@@ -41,8 +47,9 @@ import javax.swing.border.LineBorder;
  *
  * @author Yuan Hao Li
  */
-public class ContestGUI extends JFrame implements Runnable{
+public class ContestGUI extends JFrame implements Runnable {
 //testo
+
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     int width = screenSize.width;
     int height = screenSize.height;
@@ -52,25 +59,27 @@ public class ContestGUI extends JFrame implements Runnable{
     public static String password = "johnny";
 
     private String question = "", a = "", b = "", c = "", d = "", answer = "", hint = "", playerAnswer = "", audience = "Ask Someone Next To You";
-    private JPanel informationPanel, questionPanel, userInputPanel, optionsPanel,prizePanel,displayPanel;
-    private JButton aButton,bButton,cButton,dButton,hintButton,audienceButton,halfButton;
-    private JLabel questionLabel,nextPrize,currentPrize,displayLabel,playerLabel;
+    private JPanel informationPanel, questionPanel, userInputPanel, optionsPanel, prizePanel, displayPanel;
+    private JButton aButton, bButton, cButton, dButton, hintButton, audienceButton, halfButton;
+    private JLabel questionLabel, nextPrize, currentPrize, displayLabel, playerLabel;
     private ImageIcon logoIcon = new ImageIcon(new ImageIcon("Logo2.png").getImage().getScaledInstance(250, 250, Image.SCALE_DEFAULT));
     private JTextField inputName;
     private String playerName;
     public boolean check = true;
-    PlayerInformationGUI playerGUI ;
+    PlayerInformationGUI playerGUI;
     Color gold = new Color(233, 139, 42);
-    Color pink = new Color(254,223,225);
-    Color gray = new Color(218,201,166);
+    Color pink = new Color(254, 223, 225);
+    Color gray = new Color(218, 201, 166);
     private int questionID = 0;
-    private int prize = 0 ,prizeGoal=100;
+    private int prize = 0, prizeGoal = 100;
+    private String[] playerArray; // top 3 players
+
     public ContestGUI() {
         super();
         playerGUI = new PlayerInformationGUI(this);
     }
 
-    public void startGame()  // generate the Game frame and connect to the database
+    public void startGame() // generate the Game frame and connect to the database
     {
         establishMySQLConnection();
         this.setSize(width / 3 * 2, height / 3 * 2);
@@ -110,11 +119,8 @@ public class ContestGUI extends JFrame implements Runnable{
             System.err.println(ex.getMessage());
         }
     }
-    
-    
 
-    public ResultSet getQA()
-    {
+    public ResultSet getQA() {
         ResultSet rs = null;
         try {
             Statement statement = conn.createStatement();
@@ -162,46 +168,59 @@ public class ContestGUI extends JFrame implements Runnable{
         displayLabel = new JLabel();
         displayLabel.setIcon(logoIcon);
         displayPanel.setLayout(new BorderLayout());
-        displayPanel.add(displayLabel,BorderLayout.CENTER);
+        displayPanel.add(displayLabel, BorderLayout.CENTER);
         displayPanel.setBackground(gray);
     }
 
     public JPanel scorePanel() { //NEEDS TO BE DONE , this display the player previous score and the top 3 
         JPanel scorePanel = new JPanel();
+        scorePanel.setLayout(new GridLayout(4, 1));
         playerLabel = new JLabel();
         playerLabel.setText("Current Player: " + playerGUI.getPlayerName());
         playerLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 44));
         scorePanel.setBackground(gray);
         scorePanel.add(playerLabel);
+        findTheTop3();
+
+        JLabel top1Label = new JLabel("Top 1 " + playerArray[0]);
+        JLabel top2Label = new JLabel("Top 2 " + playerArray[1]);
+        JLabel top3Label = new JLabel("Top 3 " + playerArray[2]);
+        top1Label.setFont(new Font("Comic Sans MS", Font.BOLD, 44));
+        top2Label.setFont(new Font("Comic Sans MS", Font.BOLD, 44));
+        top3Label.setFont(new Font("Comic Sans MS", Font.BOLD, 44));
+
+        scorePanel.add(top1Label);
+        scorePanel.add(top2Label);
+        scorePanel.add(top3Label);
+
         return scorePanel;
     }
 
-  public void prizePanel() {
+    public void prizePanel() {
         prizePanel = new JPanel();
-        currentPrize = new JLabel("Current Prize: "+String.valueOf(prize));
-        nextPrize = new JLabel("Prize Goal: "+String.valueOf(prizeGoal));
+        currentPrize = new JLabel("Current Prize: " + String.valueOf(prize));
+        nextPrize = new JLabel("Prize Goal: " + String.valueOf(prizeGoal));
         currentPrize.setFont(new Font("Comic Sans MS", Font.BOLD, 44));
         nextPrize.setFont(new Font("Comic Sans MS", Font.BOLD, 44));
 
-        prizePanel.setLayout(new GridLayout(2,1));
+        prizePanel.setLayout(new GridLayout(2, 1));
         prizePanel.setBackground(gray);
         prizePanel.add(currentPrize);
         prizePanel.add(nextPrize);
     }
 
-
-    public void questionPanel() { 
+    public void questionPanel() {
         questionPanel = new JPanel();
         questionPanel.setBackground(gray);
         questionPanel.setLayout(new BorderLayout());
-       
+
         questionLabel = new JLabel();
 
         questionLabel.setText(question);
-        questionLabel.setPreferredSize(new Dimension(width , height/3));
+        questionLabel.setPreferredSize(new Dimension(width, height / 3));
         questionLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 44));
         questionLabel.setForeground(Color.black);
-        
+
         questionPanel.add(questionLabel);
     }
 
@@ -210,13 +229,13 @@ public class ContestGUI extends JFrame implements Runnable{
         optionsPanel = new JPanel();
         optionsPanel.setBackground(gray);
         optionsPanel.setLayout(new GridLayout(2, 2, 20, 20));
-         aButton = new JButton(a);
+        aButton = new JButton(a);
         bButton = new JButton(b);
         cButton = new JButton(c);
-         dButton = new JButton(d);
+        dButton = new JButton(d);
 
         optionButtonsMouseListener optionButtonListener = new optionButtonsMouseListener();
-        
+
         aButton.setFont(new Font("Comic Sans MS", Font.BOLD, 40));
         aButton.setBackground(Color.white);
         aButton.setForeground(Color.black);
@@ -237,7 +256,7 @@ public class ContestGUI extends JFrame implements Runnable{
         dButton.setForeground(Color.black);
         dButton.setBorder(new LineBorder(Color.BLACK, 4));
         dButton.addMouseListener(optionButtonListener);
-        
+
         optionsPanel.add(aButton);
         optionsPanel.add(bButton);
         optionsPanel.add(cButton);
@@ -257,23 +276,21 @@ public class ContestGUI extends JFrame implements Runnable{
             if (check) {
                 getQA();
                 questionLabel.setText(question);
-                aButton.setText(a);  
+                aButton.setText(a);
                 bButton.setText(b);
                 cButton.setText(c);
                 dButton.setText(d);
                 questionPanel.revalidate();
-                
-                currentPrize.setText("Current Prize: "+ String.valueOf(prize));
-                nextPrize.setText("Prize Goal: "+String.valueOf(prizeGoal));
+
+                currentPrize.setText("Current Prize: " + String.valueOf(prize));
+                nextPrize.setText("Prize Goal: " + String.valueOf(prizeGoal));
                 prizePanel.revalidate();
-                
+
                 displayLabel.setText("");
                 displayLabel.setIcon(logoIcon);
                 displayPanel.revalidate();
-               
-            }
-            else
-            {
+
+            } else {
                 exitGame();
             }
         }
@@ -304,30 +321,29 @@ public class ContestGUI extends JFrame implements Runnable{
             jb.setBorder(new LineBorder(Color.black, 4));
         }
     }
-    private class helpButtonsMouseListener implements MouseListener  // have not done the Audience and half half yet 
+
+    private class helpButtonsMouseListener implements MouseListener // have not done the Audience and half half yet 
     {
 
         JButton jb;
+
         @Override
         public void mouseClicked(MouseEvent e) {
-            jb = (JButton) e.getSource(); 
-            if(jb.equals(hintButton))
-            {
+            jb = (JButton) e.getSource();
+            if (jb.equals(hintButton)) {
                 System.out.println("Hint Pressed");
-                
+
                 displayPanel.removeAll();
                 displayLabel = new JLabel(hint);
                 displayLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 30));
                 displayPanel.add(displayLabel);
                 displayPanel.revalidate();
-            }
-            
-            else if (jb.equals(audienceButton)) // display audience panel
+            } else if (jb.equals(audienceButton)) // display audience panel
             {
                 System.out.println("Auidence Pressed");
-                
+
                 displayPanel.removeAll();
-                displayLabel = new JLabel(audience); 
+                displayLabel = new JLabel(audience);
                 displayLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 30));
                 displayPanel.add(displayLabel);
                 displayPanel.revalidate();
@@ -342,8 +358,8 @@ public class ContestGUI extends JFrame implements Runnable{
         public void mouseReleased(MouseEvent e) {
         }
 
-         @Override
-         public void mouseEntered(MouseEvent e) {
+        @Override
+        public void mouseEntered(MouseEvent e) {
             jb = (JButton) e.getSource();
             jb.setBackground(Color.black);
             jb.setForeground(gold);
@@ -358,13 +374,14 @@ public class ContestGUI extends JFrame implements Runnable{
             jb.setBorder(new LineBorder(Color.black, 4));
         }
     }
+
     public JPanel helpPanel() {
         JPanel helpPanel = new JPanel();
         helpPanel.setBackground(gray);
         hintButton = new JButton("     Hint     ");
         audienceButton = new JButton("     Audience     ");
         halfButton = new JButton("     Half Half     ");
-        
+
         helpButtonsMouseListener helpButtonsMouseListener = new helpButtonsMouseListener();
 
         hintButton.setFont(new Font("Comic Sans MS", Font.BOLD, 40));
@@ -383,8 +400,6 @@ public class ContestGUI extends JFrame implements Runnable{
         halfButton.setBorder(new LineBorder(Color.BLACK, 4));
         halfButton.addMouseListener(helpButtonsMouseListener);
 
-        
-        
         helpPanel.setLayout(new GridLayout(3, 1, 0, 20));
         helpPanel.add(hintButton);
         helpPanel.add(audienceButton);
@@ -396,36 +411,159 @@ public class ContestGUI extends JFrame implements Runnable{
         return playerAnswer == answer.charAt(0);
     }
 
-    public void exitGame()
-    {
-        System.out.println("You got : "+prize +"    Your goal :"+prizeGoal);
+    public void exitGame() {
+        System.out.println("You got : " + prize + "    Your goal :" + prizeGoal);
         outputTheScore(prize);
         System.exit(0);
         this.dispose();
         this.setVisible(false);
     }
-    
-    public void outputTheScore(int score)
-    {
-        try{
-         PrintWriter outputStream = new PrintWriter(new FileOutputStream("PlayerInformation.txt",true));
-         outputStream.println(" "+score);
-         outputStream.close();
-        }catch(FileNotFoundException e){
-           System.out.println("Error opening the file out.txt."+ e.getMessage());
+
+    public void outputTheScore(int score) {
+        try {
+            PrintWriter outputStream = new PrintWriter(new FileOutputStream("PlayerInformation.txt", true));
+            outputStream.println(":" + score);
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error opening the file out.txt." + e.getMessage());
         }
 
     }
-    public void prizeCount(int questionNo)  // start from 100 , next question is a double the prize of the privous one 
+
+    public void prizeCount(int questionNo) // start from 100 , next question is a double the prize of the privous one 
     {
-        prize= 0 ;
-        prizeGoal= 100;
-        for(int i = 1;i <questionNo; i++)
-        {
-            prize+=prizeGoal;
-            prizeGoal *=2;
+        prize = 0;
+        prizeGoal = 100;
+        for (int i = 1; i < questionNo; i++) {
+            prize += prizeGoal;
+            prizeGoal *= 2;
         }
     }
+
+    public void findTheTop3() {
+        int top1=0, top2=0, top3=0;
+        ArrayList<Integer> scoreList = new ArrayList<Integer>();
+        playerArray = new String[3];
+
+        //try to find the best 3 player's scores 
+        try {
+            FileReader fr = new FileReader("PlayerInformation.txt");
+            BufferedReader inputStream = new BufferedReader(fr);
+            String line = null;
+            while ((line = inputStream.readLine()) != null) {
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == ':') // for example  Johnny Li,1000   : comma behind is score
+                    {
+                        try {
+                            System.out.println(line.substring(0, line.length()));
+                            Integer score = Integer.valueOf(line.substring(i + 1, line.length()));
+                            scoreList.add(score);
+                        } catch (NumberFormatException e) {
+                            System.err.println(e);
+                        }
+                    }
+                }
+
+            }
+            inputStream.close();
+
+        } catch (IOException e) {
+            System.out.println("File  not found.");
+        }
+
+        
+        Collections.sort(scoreList);//BUG if its below 3 players 
+      for(int i = 0 ;i<scoreList.size();i++)
+        {
+            System.out.println("Score : "+i +":  "+scoreList.get(i));
+        }
+      
+      
+        if (!scoreList.isEmpty()) {
+            top1 = scoreList.get(scoreList.size()-1);
+            if (scoreList.size()>1) {
+                top2 = scoreList.get(scoreList.size()-2);
+                if (scoreList.size()>2) {
+                    top3 = scoreList.get(scoreList.size()-3);
+                }
+            }
+
+        }
+        // try to find the best three player's name 
+        try {
+            FileReader fr = new FileReader("PlayerInformation.txt");
+            BufferedReader inputStream = new BufferedReader(fr);
+            String line = null;
+              int round =0 ;
+            while ((line = inputStream.readLine()) != null) {
+                for (int i = 0; i < line.length(); i++) {
+                    if (line.charAt(i) == ':') // for example  Johnny Li,1000   : comma behind is score
+                    {
+                        Integer score = Integer.valueOf(line.substring(i + 1, line.length()));
+                        
+                        if(top1 == top2&&top1 == top3) 
+                        {
+                            if(round<3)
+                            {
+                                playerArray[round] = line;
+                                round++;
+                                System.out.println(round + "Round top 1 == top2 == top3 ");
+                                continue;
+                            }
+                        }else if(top1 == top2)
+                        {
+                            if(round<2)
+                            {
+                                playerArray[round] = line;
+                                round++;
+                                System.out.println(round + "Round top 1 == top2 ");
+                                continue;
+                            }
+                        }
+                       
+                
+                        
+                        if(top2 == top3)
+                        {
+                          if(round<2)
+                          {
+                          playerArray[round+1] = line;
+                          System.out.println(round+1 + "Round top 2 == top3 ");
+                          round++;
+                          continue;
+                          }
+                  
+                        }
+                        
+      // ==================================================================================BUG ===============================
+                          if (score == top1) {
+                            playerArray[0] = line;
+                        }
+                        if(score == top2)
+                        {
+                            playerArray[1] = line;
+                        }
+                        if(score == top3)
+                        {
+                            playerArray[2] = line;
+                        }
+                    }
+                }
+
+            }
+            inputStream.close();
+
+        } catch (IOException e) {
+            System.out.println("File  not found.");
+        }
+        
+        for (int i = 0; i < playerArray.length; i++) {
+            System.out.println(i+" :"+playerArray[i]);
+        }
+
+
+    }
+
     public static void main(String[] args) {
 
         ContestGUI contestFrame = new ContestGUI();
